5887569c8dc4ba6cbee41f2b02b850c77a1394ca
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-03 12:14:17 +0100

    add astro integration

--- src/astro/index.js
+++ src/astro/index.js
@@ -1,1 +1,16 @@
 import WildcardResolverVitePlugin from "../vite/index.js";
+const AstroWildcardResolver = ({extensions = []} = {}) => ({
+    name: "astro-wildcard-resolvee",
+    hooks: {
+        "astro:config:setup": async ({ updateConfig }) => {
+          updateConfig({
+            vite : {
+              plugins : [
+                WildcardResolverVitePlugin({extensions})
+              ]
+            }
+          })
+        },
+    },
+});
+export default AstroWildcardResolver;
