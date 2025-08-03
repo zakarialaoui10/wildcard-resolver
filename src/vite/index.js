84c9a9b11c972c612b4cc10207f97a1f12a9a00c
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-03 12:51:14 +0100

    fix vite

--- src/vite/index.js
+++ src/vite/index.js
@@ -2,15 +2,15 @@ export default function WildcardResolverVitePlugin({extensions = []}={}) {
   return {
     name: 'wildcard-resolver-vite-plugin',
     enforce: 'pre',
-    resolveId(source) {
-      const ext = extensions.find(e => source.endsWith(e));
-      if (ext) return source;
-      return null;
-    },
-    load(id) {
+    transform(code, id) {
       const ext = extensions.find(e => id.endsWith(e));
       if (ext) {
-        return `export default ${JSON.stringify(require('fs').readFileSync(id, 'utf-8'))};`;
+        return {
+            code : `
+const Content = ${JSON.stringify(code)}
+export default Content
+            `.trim()
+        }
       }
       return null;
     },
